59d35612e37dc37545ae307414e2ca202913eeca
Stefan Poibrenski <stefan_poibrenski@abv.bg>
2023-01-15 19:47:34 +0200

    feat: add logo component

---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+
+export default function Logo({ type }) {
+	return <img src={type === 'default' ? "/images/logo.svg" : "/images/logo-muted.svg"} alt="logo-image" />
+}
+
+Logo.defaultProps = {
+	type: 'default'
+}
